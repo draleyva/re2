d5a8c8fdb7065a565755b3af46436af7dd4c2ea4
David Ricardo Leyva Jiménez <draleyva@gmail.com>
2017-04-10 17:28:55 -0500

    Adding re2

---
+++ RE2Services.cpp
@@ -0,0 +1,8 @@
+#include <re2/filtered_re2.h>
+#include "RE2Services.h"
+
+using namespace re2;
+
+RE2Services::RE2Services()
+{
+}
---
+++ RE2Services.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class RE2Services
+{
+public:
+  RE2Services();
+};
+
